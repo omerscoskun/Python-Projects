8c48fd919537c2789e400e35610f6cbbab400f1f
omerscoskun <omersaitcoskun@gmail.com>
2025-11-02 17:07:08 +0300

    Yeni Proje Eklendi: Matrix Similarity

--- Projects/Similarity Algorithms/Matrix Similarity/iki_resim_arasinda_fark_bulma.py
+++ Projects/Similarity Algorithms/Matrix Similarity/iki_resim_arasinda_fark_bulma.py
@@ -1,5 +1,7 @@
 # region Imports
 
+import math
+from tkinter import Image
 import cv2
 import warnings
 import numpy as np
@@ -22,7 +24,25 @@ def pixelFark(pixel_1, pixel_2):
         fark += abs(pixel_1[i] - pixel_2[i]) / 255
     return fark 
 
+def farkMatris(matris1, matris2):
 
+    yukseklik, genislik = matris1.shape[0], matris1.shape[1]
+    matris = np.zeros(shape=(yukseklik, genislik, 3), dtype=np.uint8)
+
+    for i in range(yukseklik):
+        for j in range(genislik):
+                if pixelFark(matris1[i][j],matris2[i][j]) == 0:
+                    matris[i,j,0] = 255
+                    matris[i,j,1] = 255
+                    matris[i,j,2] = 255
+                    continue
+                else:
+                    matris[i,j,0] == matris2[i][j][0]
+                    matris[i,j,1] == matris2[i][j][1]
+                    matris[i,j,2] == matris2[i][j][2]
+                    
+    img = Image.fromarray(matris, 'RGB')
+    img.save('farkMatris.png')
 
 # endregion
 
@@ -49,6 +69,9 @@ for i in range(yukseklik):
 farklilik_oran = 100 * fark / (genislik * yukseklik * 3)
 print("İki görsel arasındaki farklılık oranı :" + str(farklilik_oran))
 
-
+benzerlik_oran = 100 - farklilik_oran
+print("İki görsel arasındaki benzerlik oranı :" + str(benzerlik_oran))
+farkMatris(gorsel_1, gorsel_2)
+print("İki görsel arasındaki farklılıklar, fark.png dosyası olarak kayıt edildi")
 
 # endregion
